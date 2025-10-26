1916bb698055e5cf7cf20c7e6953cc7fccda6fc0
jstruongg <nguyenqtthangbinh@gmail.com>
2025-10-26 14:39:44 +0700

    update full

--- DATN_FE/src/router/index.js
+++ DATN_FE/src/router/index.js
@@ -11,6 +11,11 @@ const routes = [
     meta: { layout: "client" },
   },
   {
+    path: "/login",
+    component: () => import("../components/Client/Login/index.vue"),
+    meta: { layout: "blank" },
+  },
+  {
     path: "/blog",
     component: () => import("../components/Client/Blog/index.vue"),
     meta: { layout: "client" },
@@ -33,6 +38,76 @@ const routes = [
   {
     path: "/notebook-detail",
     component: () => import("../components/Client/NotebookDetail/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/contact",
+    component: () => import("../components/Client/Contact/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/recruitment",
+    component: () => import("../components/Client/Recruitment/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/programme",
+    component: () => import("../components/Client/Programme/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/middle-programme",
+    component: () => import("../components/Client/MiddleProgram/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/meal",
+    component: () => import("../components/Client/Meal/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/instructPayment",
+    component: () => import("../components/Client/InstructPayment/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/tuition",
+    component: () => import("../components/Client/Tuition/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/registration",
+    component: () => import("../components/Client/Registration/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/activities",
+    component: () => import("../components/Client/Activities/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/advantages",
+    component: () => import("../components/Client/Advantage/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/parent-mail-box",
+    component: () => import("../components/Client/ParentMailbox/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/paymentHistory",
+    component: () => import("../components/Client/PaymentHistory/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/privacy",
+    component: () => import("../components/Client/Privacy/index.vue"),
+    meta: { layout: "client" },
+  },
+  {
+    path: "/study-schedule",
+    component: () => import("../components/Client/StudySchedule/index.vue"),
     meta: { layout: "client" },
   },
 ];
