5fb26390d95eca1e21947fe309a6148cf6b34bfb
jstruongg <nguyenqtthangbinh@gmail.com>
2025-11-29 09:00:14 +0700

    update feature 1.1.4

--- DATN_FE/src/main.js
+++ DATN_FE/src/main.js
@@ -1,10 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Default from './layout/wrapper/index.vue'
+import ClientLayout from './layout/Client/wrapper/index.vue'
+import BlankLayout from './layout/Blank/wrapper/index.vue'
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: { defaultSet: 'mdi' }
+})
+
 const app = createApp(App)
 
 app.use(router)
-app.component("default-layout", Default);
+app.use(vuetify)
+app.component('client-layout', ClientLayout)
+app.component('blank-layout', BlankLayout)
+app.component('default-layout', ClientLayout)
 
-app.mount("#app")
+app.mount('#app')
